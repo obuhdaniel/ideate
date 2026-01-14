8d97264ea029b2c8a1452cafb210c3cd5cf26419
xavierscript <david111francisco@gmail.com>
2026-01-14 13:39:29 +0100

    finished nav

--- components/layout/Navigation.tsx
+++ components/layout/Navigation.tsx
@@ -2,16 +2,16 @@ export default function Navigation() {
   return (
     <>
       <nav className="fixed top-0 left-0 right-0 z-50 flex items-center justify-center px-8 mb-16 bg-transparent">
-        <div className="flex items-center justify-between w-full max-w-6xl px-8 py-6">
+        <div className="flex items-center mx-auto gap-32 w-fit px-8 py-6">
           <a
-            href="#home"
-            className="text-white text-sm font-light hover:text-purple-400 transition-colors duration-300 hover:scale-105"
+            href="#process"
+            className="text-white text-lg font-light hover:text-purple-400 transition-colors duration-300 hover:scale-105"
           >
-            Home
+            Process
           </a>
           <a
             href="#services"
-            className="text-white text-sm font-light hover:text-purple-400 transition-colors duration-300 hover:scale-105"
+            className="text-white text-lg font-light hover:text-purple-400 transition-colors duration-300 hover:scale-105"
           >
             Services
           </a>
@@ -19,19 +19,19 @@ export default function Navigation() {
           <img
             src="/images/hero/ideate.png"
             alt="Ideate Logo"
-            width={100}
+            width={140}
             height={50}
           />
 
           <a
             href="#portfolio"
-            className="text-white text-sm font-light hover:text-purple-400 transition-colors duration-300 hover:scale-105"
+            className="text-white text-lg font-light hover:text-purple-400 transition-colors duration-300 hover:scale-105"
           >
             Portfolio
           </a>
           <a
             href="#contact"
-            className="text-white text-sm font-light hover:text-purple-400 transition-colors duration-300 hover:scale-105"
+            className="text-white text-lg font-light hover:text-purple-400 transition-colors duration-300 hover:scale-105"
           >
             Contact us
           </a>
